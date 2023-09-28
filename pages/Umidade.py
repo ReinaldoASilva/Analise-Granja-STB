f868e9f94bda7dc06de946f742186e1593b50f74
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-09-28 20:20:03 -0300

    Update Umidade.py

--- pages/Umidade.py
+++ pages/Umidade.py
@@ -5,16 +5,11 @@ import plotly.express as px
 import requests
 import io
 
-# carregar arquivo
-#umidade = pd.read_csv('/Users/reinaldoblack/Documents/documentos/Sitio-Balão/Analise-Granja-STB/smaai_leituras_atualizado.csv')
-
 url = 'https://raw.githubusercontent.com/ReinaldoASilva/Analise-Granja-STB/main/smaai_leituras_atualizado.csv?token=GHSAT0AAAAAACHY4PQKYCARKKDW5M44DSROZIWBFGA'
 response = requests.get(url)
 content = response.content
 
 umidade = pd.read_csv(io.BytesIO(content))
-
-
 
 submenu_umidade = ['Análise de Umidade', 'Análise por Período', 'Pico de Umidade']
 subpagina_selecionada = st.sidebar.radio('Umidade',submenu_umidade)
