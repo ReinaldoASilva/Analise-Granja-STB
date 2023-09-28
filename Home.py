22b03aa9cce21064be806d696fcaddf5b6622843
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-09-28 06:23:46 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ st.write("Este projeto de análise de dados tem como objetivo investigar como as
 Analisaremos os momentos em que a temperatura e umidade atingem niveis extremos, buscando compreender como esses fatores influenciam na conversão alimentar..\
 A ideia é buscar melhorias nas práticas de manejo e identificar os intervalos perfeitos de temperatura e umidade, com o objetivo de maximizar os lucros nas granjas analisadas.")
 
-st.image("https://github.com/ReinaldoASilva/Analise-Granja-STB/blob/main/producao-avicola.jpg?raw=true)
+st.image("https://github.com/ReinaldoASilva/Analise-Granja-STB/blob/main/producao-avicola.jpg?raw=true")
 
 
 
