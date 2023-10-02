2199c74a6279dbce191a9a89fe58ba9cd24245d0
Reinaldo Silva <reinaldoblack@hotmail.com>
2023-10-02 14:09:08 -0300

    insight

--- pages/3.Insight.py
+++ pages/3.Insight.py
@@ -1,14 +1,35 @@
 import streamlit as st
-
+from pathlib import Path
+from PIL import Image
+import pandas as pd
 
 # Modo responsivo
 st.set_page_config(layout="wide")
 
-# Definindo as opções do submenu de Temperatura
-submenu_umidade = ['Análise de Umidade', 'Análise por Período', 'Pico de Umidade']
-subpagina_selecionada = st.sidebar.radio('3.Insight',submenu_umidade)
+# Imagem
+current_dir = Path(__file__).parent if '__file__' in locals() else Path.cwd()
+data = current_dir / 'insight.jpeg'
+image = Image.open(data)
+
+container = st.container()
+container.markdown(
+    f"""
+    <style>
+    .center {{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+container.image(image, use_column_width=True, caption='')
 
 
+
+st.title("Insight")
 
 st.write("A umidade no aviário foi um problema durante o tempo do lote, \
         houve 24 dias consecutivos com picos e um total de 27 dias com picos no geral. Já a temperatura também foi um desafio, \
@@ -17,12 +38,15 @@ st.write("A umidade no aviário foi um problema durante o tempo do lote, \
         com temperatura acima dos limites recomendados. Além disso, a umidade chegou a atingir picos de 99%, o que agrava ainda mais a situação. \
         Nesse contexto, é essencial tomar medidas para proporcionar um ambiente o mais confortável possível para as aves.")
 
-st.write("Algumas medias que podem ser tomadas para um controle mais eficiente da ambiência")
-st.write("Medidas para baixar a umidade do aviário:")
-st.write("- Ventilação adequada: Garanta uma boa circulação de ar no aviário para ajudar a reduzir a umidade, permitindo a saída do ar úmido e a entrada de ar fresco.")
-st.write("- Instalação de janelas.")
-st.write("- Utilização de ventiladores.")
-st.write("- Utilização de exaustores.")
+st.write(" ➡️ : Algumas medias que podem ser tomadas para um controle mais eficiente da ambiência")
+st.write(" - Ventilação adequada: Garanta uma boa circulação de ar no aviário para ajudar a reduzir a umidade, permitindo a saída do ar úmido e a entrada de ar fresco.")
+st.write(" - Instalação de Janelas.")
+st.write(" - Instalação de inlet")
+st.write(" - Utilização de Ventiladores.")
+st.write(" - Utilização de Exaustores.")
+st.write(" - Isolamento Térmico")
+st.write(" - Resfriamento Evaporativo")
+
 
 
 
